c1e152f7a48c74636b0bcb43530bdfa2043baf3f
who cares <mariaaramirez80@gmail.com>
2022-12-27 18:31:42 -0800

    gets notes from db to render

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -124,4 +124,19 @@ var renderNoteList = function (notes) {
   
     $noteList.append(noteListItems);
   };
+
+// Gets notes from the db and renders them to the sidebar
+var getAndRenderNotes = function () {
+    return getNotes().then(function (data) {
+      renderNoteList(data);
+    });
+  };
   
+  $saveNoteBtn.on("click", handleNoteSave);
+  $noteList.on("click", ".list-group-item", handleNoteView);
+  $newNoteBtn.on("click", handleNewNoteView);
+  $noteList.on("click", ".delete-note", handleNoteDelete);
+  $noteTitle.on("keyup", handleRenderSaveBtn);
+  $noteText.on("keyup", handleRenderSaveBtn);
+
+  getAndRenderNotes();
