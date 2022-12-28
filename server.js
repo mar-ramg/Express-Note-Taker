e97f873c957912b942d71812c96e1ee4286b4b2e
who cares <mariaaramirez80@gmail.com>
2022-12-27 18:51:57 -0800

    listening for index and notes

--- server.js
+++ server.js
@@ -11,3 +11,10 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
