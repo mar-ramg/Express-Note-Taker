2bb108557d0bf17ab3fd8105ab635422ef48f581
who cares <mariaaramirez80@gmail.com>
2022-12-27 18:54:12 -0800

    get, post, delete API endpoints

--- server.js
+++ server.js
@@ -18,3 +18,37 @@ app.get("/", function (req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
+
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+
+        let highestId = 99;
+
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+
+        newNote.id = highestId + 1;
+
+        database.push(newNote)
+
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+        res.json(newNote);
+    });
+
