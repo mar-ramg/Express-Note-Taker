3304a6f6fc9e1811da5f9f2354174f4e8323328b
who cares <mariaaramirez80@gmail.com>
2022-12-27 18:49:56 -0800

    link to public and data parsing

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const database = require("./db/db")
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
